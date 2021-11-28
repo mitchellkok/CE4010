f82fe0370dd59b740ba57fb6a563957e283479e2
mitchellkok <mitchellkokme@gmail.com>
2021-11-28 17:46:39 +0800

    added comments for fernet.py

--- fernet.py
+++ fernet.py
@@ -3,24 +3,27 @@ from cryptography.fernet import Fernet
 import csv
 
 def sym_key_gen(symkey_fn):
-    key = Fernet.generate_key()    # key generation
+    key = Fernet.generate_key()    # generate new key
     
-    # string the key in a file
+    # write key to .key file
     with open(symkey_fn, 'wb') as symkey_file:
         symkey_file.write(key)  
 
 
 def sym_file_encrypt(symkey_fn, target_fn):
-    with open(symkey_fn, 'rb') as symkey_file:    # opening the key
+    # open key file
+    with open(symkey_fn, 'rb') as symkey_file:    
         key = symkey_file.read()
+
+    fernet = Fernet(key)    # initialise fernet object using key
     
-    fernet = Fernet(key)    # using the generated key
-    with open(target_fn, 'rb') as file:    # opening the original file to encrypt
+    # open target file
+    with open(target_fn, 'rb') as file:
         original = file.read()
         
-    encrypted = fernet.encrypt(original)    # encrypting the file
+    encrypted = fernet.encrypt(original)    # encrypt the data
     
-    # opening the file in write mode and writing the encrypted data
+    # write encrypted data to file
     with open(target_fn, 'wb') as enc_file:
         enc_file.write(encrypted)
         enc_file.close()
@@ -28,56 +31,65 @@ def sym_file_encrypt(symkey_fn, target_fn):
 
 
 def sym_file_decrypt(symkey_fn, target_fn):
-    with open(symkey_fn, 'rb') as symkey_file:    # opening the key
+    # open key file
+    with open(symkey_fn, 'rb') as symkey_file:
         key = symkey_file.read()
+    
+    fernet = Fernet(key)    # initialise fernet object using key
 
-    fernet = Fernet(key)    # using the key
-    with open(target_fn, 'rb') as enc_file:    # opening the encrypted file
+    # open ciphertext file and read data
+    with open(target_fn, 'rb') as enc_file:
         encrypted = enc_file.read()
     
-    decrypted = fernet.decrypt(encrypted)    # decrypting the file
+    decrypted = fernet.decrypt(encrypted)    # decrypt the data
     
-    # opening the file in write mode and writing the decrypted data
+    # write decrypted data to file
     with open(target_fn, 'wb') as dec_file:
         dec_file.write(decrypted)
-        enc_file.close()
+        dec_file.close()
     return dec_file
     
 
 def fernet_read_file(symkey_fn, target_fn):
-    with open(symkey_fn, 'rb') as symkey_file:    # opening the key
+    # open key file
+    with open(symkey_fn, 'rb') as symkey_file:
         key = symkey_file.read()
-    fernet = Fernet(key)    # using the key
+    fernet = Fernet(key)    # initialise fernet object using key
 
+    # open ciphertext file and read data
     with open(target_fn, 'r') as file:
         encFile = bytes(file.read(), 'utf-8')
 
-    decFile = fernet.decrypt(encFile).decode()
-    reader = csv.reader(decFile.splitlines())
+    decFile = fernet.decrypt(encFile).decode()  # decrypt data
+    reader = csv.reader(decFile.splitlines())   # parse as csv
     return reader
 
 
 def fernet_write_file(symkey_fn, target_fn, new_string):
-    with open(symkey_fn, 'rb') as symkey_file:    # opening the key
+    # open key file
+    with open(symkey_fn, 'rb') as symkey_file:
         key = symkey_file.read()
-    fernet = Fernet(key)    # using the key
+    fernet = Fernet(key)    # initialise fernet object using key
 
     new_string = new_string.lstrip()    # remove leading newline
-    encFile = fernet.encrypt(bytes(new_string, 'utf-8'))
+    encFile = fernet.encrypt(bytes(new_string, 'utf-8'))    # encrypt data
 
+    # open ciphertext file and write data
     with open(target_fn, 'w') as file:
         file.write(encFile.decode("utf-8"))
 
 
 def fernet_verify_file(symkey_fn, target_fn):
-    with open(symkey_fn, 'rb') as symkey_file:    # opening the key
+    # open key file
+    with open(symkey_fn, 'rb') as symkey_file:
         key = symkey_file.read()
-    fernet = Fernet(key)    # using the key
+    fernet = Fernet(key)    # initialise fernet object using key
 
+    # open ciphertext file and read data
     with open(target_fn, 'r') as file:
         encFile = bytes(file.read(), 'utf-8')
 
-    fernet.extract_timestamp(encFile)
+    fernet.extract_timestamp(encFile)   # verify data - throws error on failure
     print("File " + target_fn + " verified!")
     
 
